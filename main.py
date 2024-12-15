c2d47940eed758d1b2221dd60be21787c33e286b
Sean Varglund <sean.varglund@gmail.com>
2024-12-15 15:05:41 +0200

    Player is rendering on a black background, and rotates in the correct direction when a or d are pressed on the keyboard

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import pygame
-from constants import (
-    SCREEN_WIDTH,
-    SCREEN_HEIGHT,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_KINDS,
-    ASTEROID_SPAWN_RATE,
-    ASTEROID_MAX_RADIUS,)
+from constants import *
+from player import Player
 
 def main()->None:
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=pygame.color.Color(0,0,0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__=="__main__":
     main()
